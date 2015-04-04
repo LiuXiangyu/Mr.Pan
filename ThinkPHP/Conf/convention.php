9cc10feac504c9b79e9045a637fc92a87f0adb6f
liujan <457590690@qq.com>
2015-04-04 16:12:05 +0800

    update database

--- ThinkPHP/Conf/convention.php
+++ ThinkPHP/Conf/convention.php
@@ -53,10 +53,10 @@ return  array(
     'DEFAULT_FILTER'        =>  'htmlspecialchars', // 默认参数过滤方法 用于I函数...
 
     /* 数据库设置 */
-    'DB_TYPE'               =>  '',     // 数据库类型
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
     'DB_HOST'               =>  '', // 服务器地址
-    'DB_NAME'               =>  '',          // 数据库名
-    'DB_USER'               =>  '',      // 用户名
+    'DB_NAME'               =>  'db_tc',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
     'DB_PWD'                =>  '',          // 密码
     'DB_PORT'               =>  '',        // 端口
     'DB_PREFIX'             =>  '',    // 数据库表前缀
