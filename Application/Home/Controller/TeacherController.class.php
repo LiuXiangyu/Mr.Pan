b52a7294a85d74c85f381992d3c47e80c49b674d
charles <liucch3@gmail.com>
2015-04-06 19:56:47 +0800

    update

--- Application/Home/Controller/TeacherController.class.php
+++ Application/Home/Controller/TeacherController.class.php
@@ -1,8 +1,49 @@
-<?
+<?php
 
 namespace Home\Controller;
 use Think\Controller;
 
-class TeacherController extends Controller {
-    public function create() {
+class TeacherController extends Controller{
+	/*
+		增加老师
+		@param POST
+			teacher_name
+			school_id
+			college_id
+	*/
+	
 
+	public function create(){	
+		$school = D("InfoSchool");
+		$school_name = $school->create();
+		if ($school_name) {
+			$this->assign("school", $school_name);
+		}
+		else {
+			$this->error($school->getError());
+		}		
+		$this->display();
+		
+		
+		/*if (IS_POST){
+			$data["teacher_name"] = I("teacher_name");
+			$data["school_id"] = I("school_id");
+			$data["college_id"] = I("college_id");
+			$data["teacher_course"] = "|";
+
+			$teacher = D("InfoTeacher");
+			$create_result = $teacher->create($data);
+
+			if ($create_result){
+				$this->success("创建成功", U("Home/Index/index"), 2);
+			}
+			else{
+
+			}
+		}
+		else{
+			this->display();
+		}*/
+	}
+}
+?>
