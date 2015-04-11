8e0668447f4ba04344e415bdc06f74d6a376405d
liuxiangyu <liuxiangyu@live.com>
2015-04-11 16:26:32 +0800

    update

--- Public/js/create.js
+++ Public/js/create.js
@@ -1,10 +1,9 @@
 var school = document.getElementById("school");
 var college = document.getElementById("college");
-//var baseurl = "/Mr.Pan/?m=Home&c=Teacher&a=create?school_id=";
-var baseurl = <?php  echo U("Home/Teacher/addTeacher"); ?>;
 function getSelectVal(url, selector) {
     ajax(url, function(data) {
-        jsondata = data.parseJSON();
+        console.log(data);
+        var jsondata = eval('(' + data + ')');
         selector.length = 0;
         for (var i = 0; i < jsondata.length; ++i) {
             var optiona = "<option value='" + jsondata[i]['id'] + "'>" + jsondata[i]['school_name'] + "</option>";
@@ -13,6 +12,6 @@ function getSelectVal(url, selector) {
     });
 }
 var handler = function() {
-    getSelectVal("" + school.value, college);
+    getSelectVal(baseurl + school.value, college);
 };
 EventUtil.addHandler(school, "change", handler);
