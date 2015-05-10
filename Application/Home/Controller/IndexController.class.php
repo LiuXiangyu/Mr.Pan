fe46690c90fd74afc71e5b139c888e16a35fe61a
liujan <457590690@qq.com>
2015-05-10 11:22:44 +0800

    update

--- Application/Home/Controller/IndexController.class.php
+++ Application/Home/Controller/IndexController.class.php
@@ -7,12 +7,13 @@ class IndexController extends Controller {
 		读取前三天的所有评论
 	*/
     public function index(){
-    	$date = date("Y-m-d H:i:s", strtotime("-3 day")); //前三天时间
+    	$date = date("Y-m-d H:i:s", strtotime("-3 year")); //前三天时间
 		$comment = M("InfoComment");
 
 		$comment_data = $comment->where("comment_time>='$date'")->limit(30)->select(); //读取前三天的30条评论
 
 		foreach($comment_data as $key => &$value){
+			$value['school_id'] = getSchoolIdByTeacherId($value['teacher_id']);
 			$value['teacher_name'] = getTeacherNameById($value['teacher_id']);
 			$value['school_name'] = getSchoolNameById($value['school_id']);
 			$value['user_name'] = getUserNameById($value['user_id']);
