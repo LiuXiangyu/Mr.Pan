cd81e6fb7e8752b2d6250f2709db0ad54803beaa
liuxiangyu <liuxiangyu@live.com>
2015-05-10 21:23:03 +0800

    update

--- Public/js/index.js
+++ Public/js/index.js
@@ -7,20 +7,20 @@ function getNewMessages(messages) {
 function putNewMessage(message) {
     var feed = document.createElement("div");
     var user = document.createElement("span");
-    user.html(message["user_name"]);
+    user.textContent = message["user_name"];
     var teacher = document.createElement("span");
-    teacher.html(message["teacher_name"]);
+    teacher.textContent = message["teacher_name"];
     var course = document.createElement("span");
-    course.html(message["course_name"]);
+    course.textContent = message["course_name"];
     var header = document.createElement("div");
     header.appendChild(user);
-    header.appendChild("评论了");
+    header.appendChild(document.createTextNode("评论了"));
     header.appendChild(teacher);
-    header.appendChild("的");
+    header.appendChild(document.createTextNode("的"));
     header.appendChild(course);
     feed.appendChild(header);
     var content = document.createElement("div");
-    content.html(message["comment_content"]);
+    content.textContent = message["comment_content"];
     feed.appendChild(content);
     document.getElementById("feeds").appendChild(feed);
 }
