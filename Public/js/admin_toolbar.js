06115633a9c5cc3b14067c827c6bed4272504294
liujan <457590690@qq.com>
2015-06-06 12:38:26 +0800

    改进
    把导航栏做成了模板，分别为home_toolbar.js和admin_toolbar.js

---
+++ Public/js/admin_toolbar.js
@@ -0,0 +1,51 @@
+
+
+document.write('<html>'+
+'<head>'+
+  '<meta http-equiv="Content-Type" content="text/html" charset="utf-8">'+
+ '<link rel="stylesheet" href="__PUBLIC__/css/normalize.css">'+
+  '<link rel="stylesheet" href="__PUBLIC__/css/general.css">'+
+ '<link rel="stylesheet" href="__PUBLIC__/css/comment.css">'+
+  '<link href="__PUBLIC__/css/bootstrap.min.css" rel="stylesheet">'+
+    '<link rel="stylesheet" type="text/css" href="__PUBLIC__/css/bootstrap.css"/>'+
+    '<link rel="stylesheet" type="text/css" href="__PUBLIC__/css/DT_bootstrap.css">'+
+    '<link rel="stylesheet" type="text/css" href="__PUBLIC__/css/style.css"/>'+
+    '<link rel="stylesheet" type="text/css" href="__PUBLIC__/css/style.css"/>'+
+    '<link rel="stylesheet" type="text/css" href="__PUBLIC__/css/style2.css"/>'+
+    '<link rel="stylesheet" type="text/css" href="__PUBLIC__/css/demo.css"/>'+
+'</head>'+
+'<body>'+
+'<div id="top-bar">'+
+      '<div class="wrap">'+
+        '<div class="top-bar-left">'+
+            '<span class="logo"><a>Mr.Pan</a></span>'+
+        '</div>'+
+        '<div class="top-bar-right">'+
+          '<ul class="nav navbar-nav navbar-right">'+
+           '<li class="dropdown">'+
+              '<a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-user"></i>' + user_name + '<b class="caret"></b></a>'+
+              '<ul class="dropdown-menu">'+
+                            '<li><a href=' + user_manage_url +'><i class="glyphicon glyphicon-user"></i> 用户管理</a></li>'+
+                            '<li><a href=' + comment_manage_url +'><i class="glyphicon glyphicon-cog"></i> 评论管理</a></li>'+
+                            '<li><a href=' + show_info_url +'><i class="glyphicon glyphicon-edit"></i> 我的资料</a></li>'+
+                        '</ul>'+
+            '</li>'+
+            '<li>'+
+              '<a  class="logout" href='  + logout_url +  '><i class="glyphicon glyphicon-off"></i> 注销</a>'+
+            '</li>'+
+
+          
+
+
+          '</ul>'+
+        '</div>'+
+      '</div>'+
+    '</div>'+
+    '<script type="text/javascript" src="__PUBLIC__/js/jquery-1.7.2.min.js"></script>'+
+    '<script type="text/javascript" src="__PUBLIC__/js/bootstrap-dropdown.js"></script>'+
+    '<script type="text/javascript" src="__PUBLIC__/js/jquery.js"></script>'+
+    '<script type="text/javascript" src="__PUBLIC__/js/jquery.dataTables.js"></script>'+
+    '<script type="text/javascript" src="__PUBLIC__/js/demo.js"></script>'+
+    '</body>'+
+    '</html>');
+
---
+++ Public/js/home_toolbar.js
@@ -0,0 +1,33 @@
+
+
+document.write('<div id="top-bar">'+
+			'<div class="wrap">' +
+				'<div class="top-bar-left">'+
+					'<form method="GET" action=' + search_url + '>'+
+						'<span class="logo"> <a href=' + index_url + '> Mr.Pan</a></span>'+
+						'<input type="search" name="keyword" class="searchkey"/>'+
+						'<input type="radio" name="searchType" value="teacher" checked="checked" />&nbsp教师'+
+						'<input type="radio" name="searchType" value="course" />&nbsp课程'+
+						
+						'<input type="submit" value="serach" class="searchbtn"/>'+
+
+					'</form>'+
+				'</div>'+
+				'<div class="top-bar-right">'+
+	
+					'<ul class="nav navbar-nav navbar-right">'+
+						'<li class="dropdown">'+
+							'<a class="dropdown-toggle" data-toggle="dropdown" href="#"><i class="glyphicon glyphicon-user"></i> ' + user_name_url + '<b class="caret"></b></a>'+
+							'<ul class="dropdown-menu">'+
+		                       	'<li><a href=' + comment_manage_url + '><i class="glyphicon glyphicon-cog"></i> 所有评论</a>'+
+		                      		'<li><a href=' + show_info_url + '><i class="glyphicon glyphicon-edit"></i> 个人资料</a></li>'+
+		                      		'<li><a href=' + follow_list_url + '><i class="glyphicon glyphicon-heart"></i> 我的关注</a>'+
+		                  	'</ul>'+
+						'</li>'+
+						'<li>'+
+						'<a href=' + logout_url + ' class="logout"><i class="glyphicon glyphicon-off"></i> 注销</a>'+
+						'</li>'+
+					'</ul>'+
+				'</div>'+
+			'</div>'+
+		'</div>');
